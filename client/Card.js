717f26b4b8c13b93ed52bb8ec306cfb6f0e5ceb5
Aina Soler <ainasoga@gmail.com>
2015-07-05 21:14:14 +0200

    Clickable cards (max3)

---
+++ client/Card.js
@@ -0,0 +1,48 @@
+/**
+ * Created by aina on 05/07/15.
+ */
+
+/*
+function Card(id, shape, number, fill){
+    this.shape = shape;
+    this.number = number;
+    this.fill = fill;
+    this.id = id;
+    this.clicked = false;
+
+    this.sprite = new PIXI.Sprite(cardbg);
+    this.sprite.scale.set(0.4);
+    this.sprite.anchor.set(0.5, 0.5);
+    this.sprite.position.x = ((i%3)+1) * pW_;
+    this.sprite.position.y = (Math.floor(i/3)+1) * pH_;
+    this.sprite.interactive = true;
+    this.sprite.on('mousedown', onCardClick);
+    this.sprite.on('touchstart', onCardClick);
+    this.sprite.on('mouseover', onCardOver);
+    this.sprite.on('mouseout', onCardOut);
+}
+
+function onCardClick(){
+    if (this.clicked){
+        this.scale.set(0.4);
+        this.inf.clicked = false;
+        --mySelectedCards;
+    }
+    else if ( mySelectedCards<3 ) {
+        this.scale.set(0.6);
+        this.inf.clicked = true;
+        ++mySelectedCards;
+    }
+}
+
+function onCardOver(){
+    if(!this.inf.clicked){
+        this.scale.set(0.5);
+    }
+}
+
+function onCardOut(){
+    if(!this.inf.clicked) {
+        this.scale.set(0.4);
+    }
+}*/
